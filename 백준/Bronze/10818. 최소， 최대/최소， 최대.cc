0405aad10cb9f88474a6156bcdee352d76e7e1ac
KIM,chae won <112717131+Grove1212@users.noreply.github.com>
2023-12-04 17:32:50 +0900

    [Bronze III] Title: 최소, 최대, Time: 524 ms, Memory: 5808 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+	int array[1000001];
+	int N;
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		cin >> array[i];
+	}
+	sort(array, array+N);
+	cout << array[0] << " " << array[N-1];
+}
