25e2e3867e9b2c97230b1a942a3e02d12f062f28
KIM,chae won <112717131+Grove1212@users.noreply.github.com>
2024-11-18 00:01:18 +0900

    [Unrated] Title: 로봇 시뮬레이션, Time: 136 ms, Memory: 16012 KB -BaekjoonHub

---
+++ Java/백준/Unrated/2174. 로봇 시뮬레이션/로봇 시뮬레이션.java
@@ -0,0 +1,110 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int A, B, N, M;
+    static Robot[] robots;
+
+    public static class Robot {
+        int x;
+        int y;
+        String direction;
+
+        Robot(int x, int y, String d) {
+            this.x = x;
+            this.y = y;
+            direction = d;
+        }
+
+        void move() {
+            if (direction.equals("N")) y++;
+            else if (direction.equals("S")) y--;
+            else if (direction.equals("E")) x++;
+            else if (direction.equals("W")) x--;
+        }
+
+        void turnLeft() {
+            if (direction.equals("N")) direction = "W";
+            else if (direction.equals("W")) direction = "S";
+            else if (direction.equals("S")) direction = "E";
+            else if (direction.equals("E")) direction = "N";
+        }
+
+        void turnRight() {
+            if (direction.equals("N")) direction = "E";
+            else if (direction.equals("E")) direction = "S";
+            else if (direction.equals("S")) direction = "W";
+            else if (direction.equals("W")) direction = "N";
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) return true; // 같은 객체를 참조하는 경우
+            if (obj == null || getClass() != obj.getClass()) return false; // null이거나 다른 클래스인 경우
+            Robot robot = (Robot) obj; // 타입 캐스팅
+            return x == robot.x && y == robot.y;
+        }
+    }
+
+    static void checkCrashedWall(int idx) {
+        Robot robot = robots[idx];
+        if (robot.x <= 0 || robot.y <= 0 || robot.x > A || robot.y > B)
+            throw new RuntimeException("Robot " + idx + " crashes into the wall");
+    }
+
+    static void checkBumpedRobot(int idx) {
+        Robot robot = robots[idx];
+        for (int i = 1; i <= N; i++) {
+            if (idx == i) continue;
+            if (robot.equals(robots[i]))
+                throw new RuntimeException("Robot " + idx + " crashes into robot "+i);
+        }
+
+    }
+
+    static void executeCommand(int idx, String command, int repeat) {
+        for(int i = 0; i<repeat; i++){
+            if (command.equals("L")) robots[idx].turnLeft();
+            else if (command.equals("R")) robots[idx].turnRight();
+            else if (command.equals("F")) {
+                robots[idx].move();
+            }
+            checkBumpedRobot(idx);
+            checkCrashedWall(idx);
+        }
+    }
+
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        A = Integer.parseInt(st.nextToken());
+        B = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        robots = new Robot[N + 1];
+        for (int i = 1; i <= N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            String direction = st.nextToken();
+            robots[i] = new Robot(x, y, direction);
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int index = Integer.parseInt(st.nextToken());
+            String command = st.nextToken();
+            int repeat = Integer.parseInt(st.nextToken());
+            try {
+                executeCommand(index, command, repeat);
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+                return;
+            }
+        }
+        System.out.println("OK");
+    }
+}
