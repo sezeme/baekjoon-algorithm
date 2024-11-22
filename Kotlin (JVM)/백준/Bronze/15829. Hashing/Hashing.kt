a37e11224ffcc7e77cf2f97f338560c5df5c0fcc
KIM,chae won <112717131+Grove1212@users.noreply.github.com>
2024-11-22 16:06:55 +0900

    [Bronze II] Title: Hashing, Time: 80 ms, Memory: 12224 KB, Score: 100 point -BaekjoonHub

--- Kotlin (JVM)/백준/Bronze/15829. Hashing/Hashing.kt
+++ Kotlin (JVM)/백준/Bronze/15829. Hashing/Hashing.kt
@@ -1,27 +1,19 @@
 import java.io.BufferedReader
 import java.io.InputStreamReader
 
-const val R = 31
 const val M = 1234567891L
 
 fun main() {
     val br = BufferedReader(InputStreamReader(System.`in`))
     val n = br.readLine().toInt()
     val alphabets = br.readLine()
-        .split("")
-        .filter { it.isNotEmpty() }
-        .map { it[0] - 'a' + 1 }
-
-
-    var sum = 0L
-    alphabets.forEachIndexed { index, it ->
-        var result = 1L
-        repeat(index) {
-            result *= R % M
-        }
-        sum += (result * it % M)
+    var res = 0L
+    var pow = 1L
+    repeat(n) {
+        res += (alphabets[it] - 'a' + 1) * pow % M
+        pow = pow * 31 % M
     }
 
-    println(sum)
+    println(res % M)
 }
 
