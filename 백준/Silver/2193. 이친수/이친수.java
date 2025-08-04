833afc0cf90ece34ef205d51cebaf08c9b4eac1f
Chewon Kim <stellaa223@gmail.com>
2025-08-04 16:41:45 +0900

    [Silver III] Title: 이친수, Time: 108 ms, Memory: 14244 KB -BaekjoonHub

---
+++ 백준/Silver/2193. 이친수/이친수.java
@@ -0,0 +1,17 @@
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        long[][] dp = new long[N][2];
+        dp[0][0] = 0;
+        dp[0][1] = 1;
+        for(int i = 1; i < N; i++) {
+            dp[i][0] = dp[i-1][0] + dp[i-1][1];
+            dp[i][1] = dp[i-1][0];
+        }
+        
+        System.out.println(dp[N-1][0] + dp[N-1][1]);
+    }
+}
