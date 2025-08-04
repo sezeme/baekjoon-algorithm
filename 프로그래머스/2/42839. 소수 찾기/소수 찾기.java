5c82bba659479238198fc925d0a7268079afa6d5
Chewon Kim <stellaa223@gmail.com>
2025-08-04 16:00:17 +0900

    [level 2] Title: 소수 찾기, Time: 346.39 ms, Memory: 92.5 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -2,37 +2,31 @@ import java.util.*;
 
 class Solution {
     String str;
-    static int[] eratostenes;
+    static boolean[] eratostenes, visited;
     static Set<Integer> set;
-    static boolean[] visited;
     public int solution(String numbers) {
         visited = new boolean[numbers.length()];
-        // 에라토스테네스의 체: 1 - 소수, -1 - 소수가 아님, 0 - 검사안함
-        eratostenes = new int[9999999];
-        for(int i = 2; i <9999999; i++){
-            if(eratostenes[i] == 0) {
-                eratostenes[i] = 1;
-                for(int j = i*2; j < 9999999; j += i) {
-                    eratostenes[j] = -1;
-                }
-            }
+        // 에라토스테네스의 체: false - 소수, true - 소수가 아님
+        eratostenes = new boolean[10000000];
+        eratostenes[0] = eratostenes[1] = true;
+        for(int i = 2; i*i <10000000; i++){
+            for(int j= i*i; j<10000000; j+=i)
+              eratostenes[j] = true;
         }
-        
+
         str = numbers;
-        
-        // 완전탐색
         set = new HashSet<>();
         dfs("");
-        
+
         return set.size();
     }
-    
+
     void dfs(String number) {
         if(number.length() >= 8) return;
-        if(!number.equals("") && eratostenes[Integer.parseInt(number)] == 1) {
+        if(!number.equals("") && !eratostenes[Integer.parseInt(number)]) {
             set.add(Integer.parseInt(number));
         }
-        
+
         for(int i = 0; i < str.length(); i++){
             // 중복 x
             if(visited[i]) continue;
