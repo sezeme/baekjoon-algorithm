aa34c128dec8c1e2bff653f9e4ba0978a562b545
Chewon Kim <stellaa223@gmail.com>
2025-08-16 00:38:28 +0900

    [level 2] Title: 올바른 괄호, Time: 19.39 ms, Memory: 76.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    boolean solution(String s) {
+        Stack<Character> stack = new Stack<>();
+        for(int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if(ch == '(') {
+                stack.push(ch);
+            } else {
+                if(stack.isEmpty()) return false;
+                stack.pop();
+            }
+        }
+        
+        if(stack.isEmpty()) return true;
+        return false;
+    }
+}
