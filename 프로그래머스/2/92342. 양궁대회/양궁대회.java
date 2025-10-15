d3b3d3db32946772ba73c98d6e866a4c63653e25
Chewon Kim <stellaa223@gmail.com>
2025-10-15 18:26:11 +0900

    [level 2] Title: 양궁대회, Time: 0.78 ms, Memory: 80.6 MB -BaekjoonHub

--- 프로그래머스/2/92342. 양궁대회/양궁대회.java
+++ 프로그래머스/2/92342. 양궁대회/양궁대회.java
@@ -1,81 +1,70 @@
 import java.util.*;
 
 class Solution {
-    int[] ryan = new int[11];
-    int[] apeach = new int[11];
-    int maxDiff = 0;
-    ArrayList<int[]> candidRyans = new ArrayList<>();
+    int[] ryan;
+    int[] apeach;
+    int n, maxDiff;
 
-    void onEnd(int scoreDiff) {
-        if (scoreDiff == 0) {
-            return;
-        }
-        if(scoreDiff > maxDiff) {
-            maxDiff = scoreDiff;
-            candidRyans = new ArrayList<>();
-            candidRyans.add(ryan.clone());
-        } else if (scoreDiff == maxDiff) {
-            candidRyans.add(ryan.clone());
-        }
-    }
-
-    int calcDiff(int idx, int diff) {
-        if(ryan[idx] > apeach[idx]) {
-            if (apeach[idx] == 0) {
-                return diff + 10 - idx;
-            } else if (apeach[idx] > 0) {
-                return diff + (10 - idx) * 2;
-            }
-        } else {
-            return diff;
-        }
-        return diff;
-    }
-
-    void dfs(int idx, int rem, int scoreDiff) {
-        if (rem<0) {return;}
-        if (rem == 0) {onEnd(scoreDiff);}
-        if (idx != 11) { 
-            for(int cnt = apeach[idx] + 1; cnt >=0; cnt--) {
-                ryan[idx] += cnt;
-                dfs(idx+1, rem - cnt, calcDiff(idx, scoreDiff));
-                ryan[idx] -= cnt;
-            }
-        } 
-    }
-
-    int apeachScore() {
-        int ret = 0;
-        for(int i = 0; i < 11; i++) {
-            if (apeach[i] > 0) {ret += 10-i;}
-        }
-        return ret;
-    }
-
-    int[] oneTrueRyan() { 
-        for (int i = 10; i>=0; i--) {
-            if(candidRyans.size()==1) {break;}
-            ArrayList<int[]> nextRyans = new ArrayList<>();
-            int maxI = 0;
-            for (int[] ryan : candidRyans) {
-                maxI = Math.max(ryan[i], maxI);
-            }
-            for(int[]ryan: candidRyans) {
-                if (ryan[i] == maxI) {
-                    nextRyans.add(ryan);
-                }
-            }
-            candidRyans = nextRyans;
-        }
-        return candidRyans.get(0);
-    }
-
+    /*
+    dfs로 끝에 도달하면 -> 이기는지 지는지 판단. 전부 지는경우밖에 없는 것을 대비해서 canWin boolean을 이길 경우 true로 변경 점수 차이가 기존 max 점수차이보다 더 나는지 판단
+    만약 더 난다면, 배열을 비교 -> 두 배열 중 한 배열 선택
+    */
     public int[] solution(int n, int[] info) {
         this.apeach = info;
-        dfs(0, n, -apeachScore());
-        if (maxDiff == 0) {
-            return new int[] {-1};
+        this.n = n;
+        dfs(new int[11], 0, 0);
+        return maxDiff > 0 ? ryan : new int[]{-1};
+    }
+    
+    void dfs(int[] arr, int depth, int idx) {
+        if(idx == 11) {
+            if(depth != n) arr[10] += n - depth;
+            int diff = compare(arr);
+            if(diff > 0) {
+                if(maxDiff == diff && isBetter(arr)) {
+                    ryan = arr.clone();
+                } else if(maxDiff < diff) {
+                    ryan = arr.clone();
+                    maxDiff = diff;
+                }
+            }
+            if(depth != n) arr[10] -= n - depth;
+            return;
         }
-        return oneTrueRyan();
+        
+        // 점수를 먹거나, 먹지 않거나
+        int need = apeach[idx] + 1;
+        if (need <= n - depth) {
+            arr[idx] = need;
+            dfs(arr, depth + need, idx + 1);
+            arr[idx] = 0;
+        }
+        
+        dfs(arr, depth, idx + 1);
+    }
+        
+    boolean isBetter(int[] arr) {
+        for(int i = 10 ; i >= 0; i--) {
+            if(arr[i] > ryan[i]) return true;
+            if(arr[i] < ryan[i]) return false;
+        }
+        return false;
+    }
+    
+    int compare(int[] arr) {
+        int ryanScore = 0;
+        int apeachScore = 0;
+        
+        for(int i = 0; i <= 10; i++) {
+            if(arr[i] == 0 && apeach[i] == 0) continue;
+            
+            if(arr[i] > apeach[i]) {
+                ryanScore += 10 - i;
+            } else {
+                apeachScore += 10 - i;
+            }
+        }
+        
+        return ryanScore - apeachScore;
     }
 }
