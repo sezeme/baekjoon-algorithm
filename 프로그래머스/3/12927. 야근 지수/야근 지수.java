af910becd300137574a1608c253238754209d386
Chewon Kim <stellaa223@gmail.com>
2026-01-22 22:31:58 +0900

    [level 3] Title: 야근 지수, Time: 121.33 ms, Memory: 70.8 MB -BaekjoonHub

---
+++ 프로그래머스/3/12927. 야근 지수/야근 지수.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Solution {
+    public long solution(int n, int[] works) {
+        PriorityQueue<Integer> q = new PriorityQueue<>(Collections.reverseOrder());
+        for(int i = 0; i < works.length; i++) {
+            q.offer(works[i]);
+        }
+        
+        while(n > 0) {
+            int work = q.poll();
+            if(work == 0) break;
+            work -= 1; 
+            q.offer(work);
+            n -= 1; 
+        }
+        
+        long answer = 0;
+        for(int work : q) {
+            answer += work * work;
+        }
+        return answer;
+    }
+}
